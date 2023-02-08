e4af124b704806ec760c23f9fd19804070f36252
kyounghwan kim <dolkys123@sju.ac.kr>
2023-02-08 16:53:12 +0900

    ...

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -37,7 +37,6 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
